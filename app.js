fe47b5f446609541fad13fee25fa9d607feb27af
Tae-yoon <yeotaeyoon@gmail.com>
2019-03-12 00:12:04 +0900

    see difference between res.send and res.josn when sending data

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-const express = require("express");
-const app = express();
+const express = require("express"),
+      app = express(),
+      port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.send("<h1>This is root route!</h1>")
+    res.send("{data: 12812938712983}");// 만약 res.sned("문자열인 객체") 이렇게 보내면 data를 html로 인식함
+    // res.json({messageL : "Hi from js object~"}); // 이건 json으로 인식함
 });
 
-app.listen(3000, () => {
-    console.log("Server has started on 3000!");
+app.listen(port, () => {
+    console.log(`Server has started on ${port}`);
 })
 
